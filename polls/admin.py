571e3707e542d10cd8de43c99e8c08846b601ab4
Ana Rey <anarey@gmail.com>
2012-10-07 17:43:23 +0200

    Re-ordering the fields on the edit form.

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,22 @@
 from polls.models import Poll, Choice
 from django.contrib import admin
 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Questions",               {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    ## campos que debe mostrar el listado de objetos del modelo.
+    list_display = ('question', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date'] ## Filtro vertial
+    search_fields = ['question'] ## Campo de busqueda sobre el parametro
+    date_hierarchy = 'pub_date'  ##  Navegacion jerarqueca en linea
+
 
 admin.site.register(Poll, PollAdmin)
-admin.site.register(Choice)
