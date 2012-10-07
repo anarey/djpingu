72996fc763f65bdcf17b63b2b04ec224b0d59aae
Ana Rey <anarey@gmail.com>
2012-10-07 17:17:17 +0200

    Re-ordering the fields on the edit form II

--- polls/admin.py
+++ polls/admin.py
@@ -2,9 +2,10 @@ from polls.models import Poll, Choice
 from django.contrib import admin
 
 class PollAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question']
-   # fields = ['question', 'pub_date']  ## Cambiar el orden de mostrado de los
-   # campos
-        
+    fieldsets = [
+        ("Questions",               {'fields': ['question']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
 admin.site.register(Poll, PollAdmin)
 admin.site.register(Choice)
